274a9e37fdc6a4558042af5ca0bf0c38d47df8c1
Sully <48493571+dizzyfrogs@users.noreply.github.com>
2026-02-26 22:44:35 -0600

    changed orientation string to block face

--- src/main/java/me/dizzyfrogs/forgottenrifts/model/Rift.java
+++ src/main/java/me/dizzyfrogs/forgottenrifts/model/Rift.java
@@ -1,6 +1,7 @@
 package me.dizzyfrogs.forgottenrifts.model;
 
 import org.bukkit.Location;
+import org.bukkit.block.BlockFace;
 
 import java.util.UUID;
 
@@ -8,17 +9,17 @@ public class Rift {
     private final UUID id;
     private final Location location;
     private final String frequency;
-    private final String orientation;
+    private final BlockFace facing;
 
-    public Rift(UUID id, Location location, String frequency, String orientation) {
+    public Rift(UUID id, Location location, String frequency, BlockFace facing) {
         this.id = id;
         this.location = location;
         this.frequency = frequency;
-        this.orientation = orientation;
+        this.facing = facing;
     }
 
     public UUID getId() { return id; }
     public Location getLocation() { return location; }
     public String getFrequency() { return frequency; }
-    public String getOrientation() { return orientation; }
+    public BlockFace getFacing() { return facing; }
 }
